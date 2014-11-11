20df5b5c416fa8b558df68617cb5202cb7fc5f35
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2014-11-11 20:57:30 +0200

    Add stub for conversation history

---
+++ src/conversation_history.c
@@ -0,0 +1,1 @@
+#include "conversation_history.h"
---
+++ test/test_conversation_history.cc
@@ -0,0 +1,14 @@
+extern "C" {
+#include "conversation_history.h"
+}
+
+#include <CppUTest/TestHarness.h>
+
+TEST_GROUP(ConversationHistoryTests)
+{
+};
+
+TEST(ConversationHistoryTests, TempTest)
+{
+    FAIL("Fail me!");
+}
