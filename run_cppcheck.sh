b2693b96b12afa9d017b9e2e16ecaaf55bf134ee
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2014-11-12 23:07:40 +0200

    Add script to run cppcheck static code analysis

---
+++ run_cppcheck.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+set -u
+
+DIR="$(readlink -f "$(dirname "$0")")"
+pushd "$DIR"
+
+cppcheck \
+    --enable=all \
+    --inconclusive \
+    --std=c99 \
+    --force \
+    --suppress=missingIncludeSystem \
+    --error-exitcode=1 \
+    --quiet \
+    -Iinclude \
+    -Isrc \
+    -I/usr/include \
+    $(pkg-config --cflags-only-I glib-2.0) \
+    $(pkg-config --cflags-only-I gtk+-2.0) \
+    $(pkg-config --cflags-only-I pidgin) \
+    .
