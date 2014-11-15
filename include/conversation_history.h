3d56519ba921232e3846d8cb132a886c665e3a5f
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2014-11-15 11:59:41 +0200

    Add copyright notices to source files

--- include/conversation_history.h
+++ include/conversation_history.h
@@ -1,3 +1,12 @@
+/**
+ * Pidgin remember read status plugin
+ * Copyright (C) 2014 Miika-Petteri Matikainen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+*/
 #ifndef CONVERSATION_HISTORY_H
 #define CONVERSATION_HISTORY_H
 
